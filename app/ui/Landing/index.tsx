efb190a53230660ebb71c8a873e9e76df4bc23e6
Wenting Y <ywtzoe@gmail.com>
2024-05-31 18:49:21 -0400

    Created animation of text and added scroll interaction

--- app/ui/Landing/index.tsx
+++ app/ui/Landing/index.tsx
@@ -1,12 +1,43 @@
 "use client";
 import Image from "next/image";
 import styles from "./style.module.scss";
-import { useRef } from "react";
+import { useRef, useLayoutEffect } from "react";
+import gsap from "gsap"; // Import the 'gsap' package
+import { ScrollTrigger } from "gsap/all";
 
 export default function Home() {
   const firstText = useRef(null);
   const secondText = useRef(null);
   const slider = useRef(null);
+  let xPercent = 0;
+  let direction = -1;
+
+  useLayoutEffect(() => {
+    gsap.registerPlugin(ScrollTrigger);
+    gsap.to(slider.current, {
+      scrollTrigger: {
+        trigger: document.documentElement,
+        scrub: 0.25,
+        start: 0,
+        end: window.innerHeight,
+        onUpdate: (e) => (direction = e.direction * -1),
+      },
+      x: "-500px",
+    });
+    requestAnimationFrame(animate);
+  }, []);
+
+  const animate = () => {
+    if (xPercent < -100) {
+      xPercent = 0;
+    } else if (xPercent > 0) {
+      xPercent = -100;
+    }
+    gsap.set(firstText.current, { xPercent: xPercent });
+    gsap.set(secondText.current, { xPercent: xPercent });
+    requestAnimationFrame(animate);
+    xPercent += 0.1 * direction;
+  };
 
   return (
     <main className={styles.landing}>
@@ -18,8 +49,8 @@ export default function Home() {
       />
       <div className={styles.sliderContainer}>
         <div ref={slider} className={styles.slider}>
-          <p ref={firstText}>Front-End Developer -</p>
-          <p ref={secondText}>Designer -</p>
+          <p ref={firstText}>UI Developer -</p>
+          <p ref={secondText}>UI Developer -</p>
         </div>
       </div>
     </main>
