4bf50c2a4aefdb7b7db59ad905b8d07a30f4f9cb
Wenting Yong <wyong@orangerine.com>
2024-05-31 11:10:03 -0400

    Resturcture skeleton of the app

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "@/app/globals.scss";
+import "@/app/ui/globals.scss";
 
 const inter = Inter({ subsets: ["latin"] });
 
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { useEffect } from "react";
-import styles from "@/app/page.module.scss";
+import styles from "@/app/ui/page.module.scss";
 
 export default function Home() {
   useEffect(() => {
