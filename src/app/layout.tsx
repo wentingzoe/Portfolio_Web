c58656c9cc4c6ecd8d74dfcd199dc9f891635b9d
Wenting Yong <wyong@orangerine.com>
2024-05-31 10:29:30 -0400

    Moved the css files into app

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "@/app/ui/globals.scss";
+import "@/app/globals.scss";
 
 const inter = Inter({ subsets: ["latin"] });
 
