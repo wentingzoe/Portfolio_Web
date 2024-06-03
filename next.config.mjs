fae74d1deb5cac465ded12c134f2dcf0842df5e1
Wenting Yong <wyong@orangerine.com>
2024-06-03 15:48:53 -0400

    Next.config file back to default

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,4 @@
-import path from "path";
 /** @type {import('next').NextConfig} */
-
-const nextConfig = {
-  sassOptions: {
-    includePaths: [path.join(process.cwd(), "styles")],
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
