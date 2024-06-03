cf59495a0a4fcf7d2aee3cafcb4df4f9a0142362
Wenting Yong <wyong@orangerine.com>
2024-06-03 15:46:08 -0400

    Created color pallete of Santa Fe and using the color value in different files

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@
 import { useEffect } from "react";
 import styles from "@/app/ui/page.module.scss";
 import Landing from "@/app/ui/Landing";
+import Header from "@/app/ui/Header";
 
 export default function Home() {
   useEffect(() => {
@@ -22,6 +23,7 @@ export default function Home() {
       <Projects />
       <SlidingImages />
       <Contact /> */}
+        <Header />
         <Landing />
       </div>
     </main>
