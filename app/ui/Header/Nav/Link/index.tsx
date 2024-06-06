029faa49e63e41f0738634aa461e0328c340b0fc
Wenting Y <ywtzoe@gmail.com>
2024-06-06 10:04:02 -0400

    Added conditional rendering based on the state

--- app/ui/Header/Nav/Link/index.tsx
+++ app/ui/Header/Nav/Link/index.tsx
@@ -4,6 +4,7 @@ import Link from "next/link";
 export default function index({ data }: { data: any }) {
   return (
     <div className={styles.link}>
+      <div className={styles.indicator}></div>
       <Link href={data.href}>{data.title}</Link>
     </div>
   );
