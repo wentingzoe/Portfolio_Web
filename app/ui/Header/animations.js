e054e579f4cb8f94a89c64f98c4f896c1107a891
Wenting Y <ywtzoe@gmail.com>
2024-06-08 09:25:34 -0400

    Created animation one for menu and one for the links

---
+++ app/ui/Header/animations.js
@@ -0,0 +1,23 @@
+export const menuSlide = {
+  initial: {
+    x: "100%",
+  },
+  enter: {
+    x: "0",
+  },
+  exit: {
+    x: "100%",
+  },
+};
+
+export const slide = {
+  initial: {
+    x: "80px",
+  },
+  enter: {
+    x: "8px",
+  },
+  exit: {
+    x: "80px",
+  },
+};
