fb1039b4913087280ed9461e47fd0a8733f7a5dd
Wenting Y <ywtzoe@gmail.com>
2024-06-19 12:54:22 -0400

    chore: Add project demo data to Projects component

--- app/ui/Projects/index.tsx
+++ app/ui/Projects/index.tsx
@@ -1,5 +1,28 @@
+"use client";
 import styles from "./style.module.scss";
 
+const projects = [
+  {
+    title: "C2 Montreal",
+    src: "c2montreal.png",
+    color: "#000000",
+  },
+  {
+    title: "Office Studio",
+    src: "officestudio.png",
+    color: "#8C8C8C",
+  },
+  {
+    title: "Locomotive",
+    src: "locomotive.png",
+    color: "#EFE8D3",
+  },
+  {
+    title: "Silencio",
+    src: "silencio.png",
+    color: "#706D63",
+  },
+];
 export default function index() {
   return <div>Project List</div>;
 }
