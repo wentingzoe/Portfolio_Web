bffba67e4ce77f46f029a3e6d3966f1b15ee7e27
Wenting Y <ywtzoe@gmail.com>
2024-06-21 10:43:12 -0400

    chore: Refactor Projects component to use useState hook and add modal functionality

--- app/ui/Projects/index.tsx
+++ app/ui/Projects/index.tsx
@@ -1,28 +1,40 @@
 "use client";
 import styles from "./style.module.scss";
+import { useState } from "react";
+import Project from "./Project"; // Import the Project component
 
-const projects = [
-  {
-    title: "C2 Montreal",
-    src: "c2montreal.png",
-    color: "#000000",
-  },
-  {
-    title: "Office Studio",
-    src: "officestudio.png",
-    color: "#8C8C8C",
-  },
-  {
-    title: "Locomotive",
-    src: "locomotive.png",
-    color: "#EFE8D3",
-  },
-  {
-    title: "Silencio",
-    src: "silencio.png",
-    color: "#706D63",
-  },
-];
-export default function index() {
-  return <div>Project List</div>;
+export default function Index() {
+  const projects = [
+    {
+      title: "C2 Montreal",
+      src: "c2montreal.png",
+      color: "#000000",
+    },
+    {
+      title: "Office Studio",
+      src: "officestudio.png",
+      color: "#8C8C8C",
+    },
+    {
+      title: "Locomotive",
+      src: "locomotive.png",
+      color: "#EFE8D3",
+    },
+    {
+      title: "Silencio",
+      src: "silencio.png",
+      color: "#706D63",
+    },
+  ];
+  const [modal, setModal] = useState({ active: false, index: 0 });
+
+  return (
+    <div className={styles.projects}>
+      <div className={styles.container}>
+        {projects.map((project, index) => {
+          return <Project key={index} project={project} setModal={setModal} />;
+        })}
+      </div>
+    </div>
+  );
 }
