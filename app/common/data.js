1c87e04be789f8772d6baa491c557360cfe83452
Wenting Y <ywtzoe@gmail.com>
2024-06-22 16:24:03 -0400

    refactor: Update data source for projects and social items

---
+++ app/common/data.js
@@ -0,0 +1,50 @@
+const projects = [
+  {
+    title: "C2 Montreal",
+    src: "c2montreal.png",
+    color: "#000000",
+    role: "Design & Development",
+    url: "",
+  },
+  {
+    title: "Office Studio",
+    src: "officestudio.png",
+    color: "#8C8C8C",
+    role: "Design & Development",
+    url: "",
+  },
+  {
+    title: "Locomotive",
+    src: "locomotive.png",
+    color: "#EFE8D3",
+    role: "Design & Development",
+    url: "",
+  },
+  {
+    title: "Silencio",
+    src: "silencio.png",
+    color: "#706D63",
+    role: "Design & Development",
+    url: "",
+  },
+];
+
+const socialItems = [
+  {
+    title: "LinkedIn",
+    href: "https://www.linkedin.com/in/wentingyong/",
+  },
+  {
+    title: "GitHub",
+    href: "https://github.com/wentingzoe",
+  },
+  {
+    title: "Email",
+    href: "mailto:ywtzoe@gmail.com",
+  },
+];
+
+module.exports = {
+  projects,
+  socialItems,
+};
--- app/ui/Projects/index.tsx
+++ app/ui/Projects/index.tsx
@@ -1,38 +1,26 @@
-"use client";
+import React from "react";
 import styles from "./style.module.scss";
 import { useState } from "react";
-import Project from "./Project"; // Import the Project component
+import Project from "./Project";
+import { projects } from "@/app/common/data";
 
 export default function Index() {
-  const projects = [
-    {
-      title: "C2 Montreal",
-      src: "c2montreal.png",
-      color: "#000000",
-    },
-    {
-      title: "Office Studio",
-      src: "officestudio.png",
-      color: "#8C8C8C",
-    },
-    {
-      title: "Locomotive",
-      src: "locomotive.png",
-      color: "#EFE8D3",
-    },
-    {
-      title: "Silencio",
-      src: "silencio.png",
-      color: "#706D63",
-    },
-  ];
   const [modal, setModal] = useState({ active: false, index: 0 });
 
   return (
     <div className={styles.projects}>
       <div className={styles.container}>
         {projects.map((project, index) => {
-          return <Project key={index} project={project} setModal={setModal} />;
+          return (
+            <div className={styles.project} key={index}>
+              <Project
+                index={index}
+                title={project.title}
+                role={project.role}
+                setModal={setModal}
+              />
+            </div>
+          );
         })}
       </div>
     </div>
