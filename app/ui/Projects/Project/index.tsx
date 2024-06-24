9ba1ba1b2a669167c62d08f7a81113a06d55a587
Wenting Y <ywtzoe@gmail.com>
2024-06-24 10:49:18 -0400

    chore: Add hover effect to project title and role in Project component; Add basic mouseEnter and mouseLeave logic in Project.

--- app/ui/Projects/Project/index.tsx
+++ app/ui/Projects/Project/index.tsx
@@ -3,9 +3,17 @@ import styles from "./style.module.scss";
 
 export default function Index({ index, title, role, setModal }) {
   return (
-    <div className={styles.project}>
+    <div
+      className={styles.project}
+      onMouseEnter={() => {
+        setModal({ active: true, index: index });
+      }}
+      onMouseLeave={() => {
+        setModal({ active: false, index: index });
+      }}
+    >
       <h2 className={styles.title}>{title}</h2>
-      <p>{role}</p>
+      <p className={styles.role}>{role}</p>
     </div>
   );
 }
