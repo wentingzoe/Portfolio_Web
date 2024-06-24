03c5836382b0aa87b112acbfb7ff562f5dc0b54a
Wenting Y <ywtzoe@gmail.com>
2024-06-24 13:43:30 -0400

    refactor: Optimize Modal component with gsap animation and mousemove event listener

--- app/ui/Projects/Modal/index.tsx
+++ app/ui/Projects/Modal/index.tsx
@@ -1,33 +1,46 @@
 "use client";
+import { useEffect, useRef } from "react";
 import styles from "./style.module.scss";
 import Image from "next/image";
 import { motion } from "framer-motion";
+import gsap from "gsap";
 import { modalScale } from "@/app/common/animations";
-interface ModalProps {
-  projects: {
-    title: string;
-    src: string;
-    color: string;
-    role: string;
-    url: string;
-  }[];
-  modal: {
-    active: boolean;
-    index: number;
-  };
-}
 
-export default function Modal({ projects, modal }: ModalProps) {
+export default function Modal({
+  projects,
+  modal,
+}: {
+  projects: any;
+  modal: any;
+}) {
   const { active, index } = modal;
+  const container = useRef(null);
+  useEffect(() => {
+    const moveContainerX = gsap.quickTo(container.current, "left", {
+      duration: 0.8,
+      ease: "power3",
+    });
+    const moveContainerY = gsap.quickTo(container.current, "top", {
+      duration: 0.8,
+      ease: "power3",
+    });
+
+    window.addEventListener("mousemove", (e) => {
+      const { clientX, clientY } = e;
+      moveContainerX(clientX);
+      moveContainerY(clientY);
+    });
+  }, []);
   return (
     <motion.div
+      ref={container}
       variants={modalScale}
       initial="initial"
       animate={active ? "enter" : "closed"}
       className={styles.container}
     >
       <div style={{ top: index * -100 + "%" }} className={styles.slider}>
-        {projects.map((project, index) => {
+        {projects.map((project: any, index: any) => {
           const { src, color, title } = project;
           return (
             <div
