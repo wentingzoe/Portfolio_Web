d89e3c6acfc4fae3baf44b0541ecc9fe9a404c81
Wenting Y <ywtzoe@gmail.com>
2024-06-24 14:24:18 -0400

    refactor: Add cursor and cursorLabel animation to Modal component

--- app/ui/Projects/Modal/index.tsx
+++ app/ui/Projects/Modal/index.tsx
@@ -15,6 +15,9 @@ export default function Modal({
 }) {
   const { active, index } = modal;
   const container = useRef(null);
+  const cursor = useRef(null);
+  const cursorLabel = useRef(null);
+
   useEffect(() => {
     const moveContainerX = gsap.quickTo(container.current, "left", {
       duration: 0.8,
@@ -24,41 +27,79 @@ export default function Modal({
       duration: 0.8,
       ease: "power3",
     });
+    const moveCursorX = gsap.quickTo(cursor.current, "left", {
+      duration: 0.5,
+      ease: "power3",
+    });
+    const moveCursorY = gsap.quickTo(cursor.current, "top", {
+      duration: 0.5,
+      ease: "power3",
+    });
+    const moveCursorLabelX = gsap.quickTo(cursorLabel.current, "left", {
+      duration: 0.45,
+      ease: "power3",
+    });
+    const moveCursorLabelY = gsap.quickTo(cursorLabel.current, "top", {
+      duration: 0.45,
+      ease: "power3",
+    });
 
     window.addEventListener("mousemove", (e) => {
       const { clientX, clientY } = e;
       moveContainerX(clientX);
       moveContainerY(clientY);
+      moveCursorX(clientX);
+      moveCursorY(clientY);
+      moveCursorLabelX(clientX);
+      moveCursorLabelY(clientY);
     });
   }, []);
   return (
-    <motion.div
-      ref={container}
-      variants={modalScale}
-      initial="initial"
-      animate={active ? "enter" : "closed"}
-      className={styles.container}
-    >
-      <div style={{ top: index * -100 + "%" }} className={styles.slider}>
-        {projects.map((project: any, index: any) => {
-          const { src, color, title } = project;
-          return (
-            <div
-              key={`modal_${index}`}
-              style={{ backgroundColor: color }}
-              className={styles.modal}
-            >
-              <Image
-                className={styles.image}
-                src={`/images/${src}`}
-                alt={title}
-                width={300}
-                height={0}
-              />
-            </div>
-          );
-        })}
-      </div>
-    </motion.div>
+    <>
+      <motion.div
+        ref={container}
+        variants={modalScale}
+        initial="initial"
+        animate={active ? "enter" : "closed"}
+        className={styles.container}
+      >
+        <div style={{ top: index * -100 + "%" }} className={styles.slider}>
+          {projects.map((project: any, index: any) => {
+            const { src, color, title } = project;
+            return (
+              <div
+                key={`modal_${index}`}
+                style={{ backgroundColor: color }}
+                className={styles.modal}
+              >
+                <Image
+                  className={styles.image}
+                  src={`/images/${src}`}
+                  alt={title}
+                  width={300}
+                  height={0}
+                />
+              </div>
+            );
+          })}
+        </div>
+      </motion.div>
+      <motion.div
+        ref={cursor}
+        variants={modalScale}
+        initial="initial"
+        animate={active ? "enter" : "closed"}
+        className={styles.cursor}
+      ></motion.div>
+      <motion.div
+        ref={cursorLabel}
+        variants={modalScale}
+        initial="initial"
+        animate={active ? "enter" : "closed"}
+        className={styles.cursorLabel}
+      >
+        <p>View</p>
+      </motion.div>
+    </>
   );
 }
