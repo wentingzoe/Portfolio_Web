bb34b5b4e424b19fe5a9b1175e7869a22bada6e4
Wenting Y <ywtzoe@gmail.com>
2024-06-26 10:07:26 -0400

    refactor: Update Header component with logo and navigation items

--- app/ui/Header/index.tsx
+++ app/ui/Header/index.tsx
@@ -4,12 +4,32 @@ import { useState } from "react";
 import { AnimatePresence } from "framer-motion";
 import Nav from "./Nav";
 import clsx from "clsx";
-
+import { navItems } from "@/app/common/data";
 export default function Home() {
   const [isActive, setIsActive] = useState(false);
 
   return (
     <>
+      <div className={styles.header}>
+        <div className={styles.logo}>
+          <span className={styles.copyright}>©</span>
+          <div className={styles.name}>
+            <p className={styles.codeBy}>Code by</p>
+            <p className={styles.fname}>Wenting</p>
+            <p className={styles.lname}>Yong</p>
+          </div>
+        </div>
+        <ul className={styles.nav}>
+          {navItems.map((item, index) => {
+            return (
+              <li key={index} className={styles.item}>
+                <a href={item.href}>{item.title}</a>
+                <span className={styles.indicator} />
+              </li>
+            );
+          })}
+        </ul>
+      </div>
       <div
         onClick={() => {
           setIsActive(!isActive);
