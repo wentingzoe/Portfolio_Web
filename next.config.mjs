9b43c320d80f767a0698bdc13b1fad7acace8609
Wenting Y <ywtzoe@gmail.com>
2024-07-19 09:38:43 -0400

    refactor: Update font-face declarations, color variables, and add common element mixins

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,7 @@ import { join } from "path";
 const nextConfig = {
   sassOptions: {
     includePaths: [join(process.cwd(), "styles")],
-    prependData: `@import "@/app/ui/variables";`,
+    prependData: `@import "@/app/ui/variables"; @import "@/app/ui/mixins"; @import "@/app/ui/fonts";`,
   },
 };
 
