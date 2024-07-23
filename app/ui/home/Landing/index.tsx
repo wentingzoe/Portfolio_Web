66b9fa40854a5b4ecf898695cef31faf8572fd9b
Wenting Y <ywtzoe@gmail.com>
2024-07-23 13:45:33 -0400

    refactor: Optimize landing page grid layout and styles

--- app/ui/home/Landing/index.tsx
+++ app/ui/home/Landing/index.tsx
@@ -6,14 +6,11 @@ export default function Home() {
   return (
     <section className={styles.landing}>
       <div className={styles.landing__container}>
-        {[...Array(32)].map((_, index) => {
-          const row = Math.floor(index / 8) + 1;
-          const column = (index % 8) + 1;
+        {[...Array(21)].map((_, index) => {
           return (
             <div
               key={index}
               className={`${styles.grid} ${styles[`grid__${index + 1}`]}`}
-              // style={{ gridArea: `${row} / ${column} / span 1 / span 1` }}
             >
               {index + 1}
             </div>
