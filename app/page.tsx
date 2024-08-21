31cfcc38571009b9af2cc4227c4ba857f59cfa18
Wenting Y <ywtzoe@gmail.com>
2024-08-20 23:06:36 -0400

    refactor: Update landing page with custom cursor and mouse position tracking

--- app/page.tsx
+++ app/page.tsx
@@ -1,28 +1,43 @@
 "use client";
-import { useEffect } from "react";
-import styles from "@/app/home/home.module.scss";
+import { useEffect, useState } from "react";
+import styles from "@/app/ui/home/home.module.scss";
 import Landing from "@/app/ui/home/Landing/index";
 import Projects from "@/app/ui/home/Projects";
+import useMousePosition from "@/app/lib/useMousePosition";
+import { motion } from "framer-motion";
 
 export default function Home() {
-  useEffect(() => {
-    (async () => {
-      const LocomotiveScroll = (await import("locomotive-scroll")).default;
-      const locomotiveScroll = new LocomotiveScroll();
-    })();
-  }, []);
+  const [isHovered, setIsHovered] = useState(false);
+
+  const { x, y } = useMousePosition();
+  const size = isHovered ? 200 : 20;
+
+  const variants = {
+    default: {
+      x: x - size / 2,
+      y: y - size / 2,
+    },
+    text: {
+      height: size,
+      width: size,
+      x: x - size / 2,
+      y: y - size / 2,
+      backgroundColor: "#f28705",
+      mixBlendMode: "difference",
+    },
+  };
 
   return (
-    <main>
-      {/* <AnimatePresence mode='wait'>
-        {isLoading && <Preloader />}
-      </AnimatePresence>
-      <Landing />
-      <Description />
-      <SlidingImages />
-      <Contact /> */}
-      <Landing />
+    <main className={styles.main}>
+      <Landing setIsHovered={setIsHovered} />
       <Projects />
+
+      <motion.div
+        className={styles.cursor}
+        variants={variants}
+        animate={isHovered ? "text" : "default"}
+        transition={{ type: "tween", ease: "backOut", duration: 0.5 }}
+      />
     </main>
   );
 }
